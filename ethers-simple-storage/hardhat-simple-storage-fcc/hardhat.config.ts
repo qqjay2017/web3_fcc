ce6826d29080fbd0b8ec0fd72f5a4b573926bc17
huangbo <qqjay2013@gmail.com>
2022-09-17 21:06:12 +0800

    hardhat api的使用11

---
+++ ethers-simple-storage/hardhat-simple-storage-fcc/hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+
+import * as dotenv from "dotenv" // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const EHTERSCAN_API_KEY = process.env.EHTERSCAN_API_KEY || ""
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: EHTERSCAN_API_KEY,
+    },
+}
+
+export default config
---
+++ ethers-simple-storage/hardhat-simple-storage-fcc/scripts/deploy.ts
@@ -0,0 +1,40 @@
+import { ethers, run, network } from "hardhat"
+import "@nomiclabs/hardhat-etherscan"
+async function main() {
+    const SimpleStorage = await ethers.getContractFactory("SimpleStorage")
+    console.log("Deploying contract")
+    const simpleStorage = await SimpleStorage.deploy()
+    await simpleStorage.deployed()
+
+    console.log(`deployed to ${simpleStorage.address}`)
+    // if (network.config.chainId !== 31337 && !!process.env.EHTERSCAN_API_KEY) {
+    //     await simpleStorage.deployTransaction.wait(6)
+    //     await verify(simpleStorage.address, [])
+    // }
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`current Value is: ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(42)
+    await transactionResponse.wait(1)
+    const updateValue = await simpleStorage.retrieve()
+    console.log(`updateValue is ${updateValue}`)
+}
+
+async function verify(contractAddress: string, args: any) {
+    console.log("verify contract ", contractAddress, args)
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            contractArguments: args,
+        })
+    } catch (e) {
+        console.log(e, "error")
+    }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+})
