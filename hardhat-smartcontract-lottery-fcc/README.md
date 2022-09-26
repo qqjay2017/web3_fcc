1819887eb76598c382b59629dc758e9602889b94
huangbo <huangbo@lj-ch.com>
2022-09-26 09:46:15 +0800

    随机数1111

---
+++ hardhat-smartcontract-lottery-fcc/README.md
@@ -0,0 +1,21 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+```
+
+View your transaction here:
+0x0181d079a7ed1118675bb9e467c11c5974695d6c997ad7201ff7fe659d9fb7a9
+
+```
+Consumers
+0xa26f2cb952d1f260288a9bd8a1abc06de5e6b498
+```
---
+++ hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+// Pragma
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Raffle_NotEnghtEthEntered();
+
+contract Raffle is VRFConsumerBaseV2 {
+  /** State Varibles */
+  uint256 private immutable i_entranceFee;
+  address payable[] private s_players;
+
+  // 随机数系列
+  VRFCoordinatorV2Interface COORDINATOR;
+
+  uint256[] public s_randomWords;
+  uint256 public s_requestId;
+  /** Events */
+  event RaffleEnter(address indexed player);
+
+  modifier onlyOwner() {
+    require(msg.sender == s_owner);
+    _;
+  }
+
+  constructor(address vrfCoordinatorV2, uint256 entranceFee)
+    VRFConsumerBaseV2(vrfCoordinatorV2)
+  {
+    i_entranceFee = entranceFee;
+    COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+  }
+
+  function enterRaffle() public payable {
+    if (msg.value < i_entranceFee) {
+      revert Raffle_NotEnghtEthEntered();
+    }
+    s_players.push(payable(msg.sender));
+    emit RaffleEnter(msg.sender);
+  }
+
+  function pickRandomWinner() external {}
+
+  // Assumes the subscription is funded sufficiently.
+  function requestRandomWords() external onlyOwner {
+    // Will revert if subscription is not set and funded.
+    s_requestId = COORDINATOR.requestRandomWords(
+      keyHash,
+      s_subscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      numWords
+    );
+  }
+
+  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+    internal
+    override
+  {
+    s_randomWords = randomWords;
+  }
+
+  function getEntranceFee() public view returns (uint256) {
+    return i_entranceFee;
+  }
+
+  function getPlayers(uint256 index) public view returns (address) {
+    return s_players[index];
+  }
+}
